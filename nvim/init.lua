83a481b213f25d16c9c469fc54a3f629b90ae31d
monji05 <monji4869@gmail.com>
2023-03-24 12:15:33 +0900

    return to not use LazyVim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,62 +1,17 @@
-require('ft')
-require('base')
-require('highlights')
-require('maps')
+require("config.options")
+require("config.keymaps")
+require("config.ft")
+require("config.lazy")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-vim.api.nvim_command [[
-  syntax off
-  filetype plugin indent off
-  filetype off
-]]
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-require('lazy').setup("plugins", {
-  change_detection = {
-    enabled = false,
-    notify = false, -- get a notification when changes are found
-  },
-  performance = {
-    chache = {
-      enabled = true
-    },
-  },
-  rtp = {
-    disabled = {
-      "gzip",
-      "matchit",
-      "matchparen",
-      "netrwPlugin",
-      "Jrplugin",
-      "tarPlugin",
-      "tohtml",
-      "tutor",
-      "zipPlugin",
-    },
-  },
-  debug = false,
-})
-
-require('theme/nightfly')
+require("theme/nightfly")
 
 local has = function(x)
   return vim.fn.has(x) == 1
 end
 
-local is_mac = has 'macunix'
+local is_mac = has("macunix")
 if is_mac then
-  require('macos')
+  require("config/macos")
 elseif is_mac == 0 then
-  require('windows')
+  require("config/windows")
 end
