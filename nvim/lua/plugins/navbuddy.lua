07658de7c0215369e91773b1c54e6be84ffe2908
monji05 <monji4869@gmail.com>
2023-07-17 08:21:49 +0900

    add navbuddy action mapping

--- nvim/lua/plugins/navbuddy.lua
+++ nvim/lua/plugins/navbuddy.lua
@@ -4,12 +4,19 @@ local kind = require("config.icons").get("kind")
 
 navbuddy.setup {
   window = {
-    border = "single", -- "rounded", "double", "solid", "none"
-    -- or an array with eight chars building up the border in a clockwise fashion
+    border = "single", -- "rounded", "double", "solid", "none" -- or an array with eight chars building up the border in a clockwise fashion
     -- starting with the top-left corner. eg: { "╔", "═" ,"╗", "║", "╝", "═", "╚", "║" }.
-    size = "70%",     -- Or table format example: { height = "40%", width = "100%"}
-    position = "50%", -- Or table format example: { row = "100%", col = "0%"}
-    scrolloff = nil,  -- scrolloff value within navbuddy window
+    size = "70%",      -- Or table format example: { height = "40%", width = "100%"}
+    position = "50%",  -- Or table format example: { row = "100%", col = "0%"}
+    scrolloff = nil,   -- scrolloff value within navbuddy window
+    node_markers = {
+      enabled = true,
+      icons = {
+        leaf = "  ",
+        leaf_selected = " → ",
+        branch = " ",
+      },
+    },
     sections = {
       left = {
         size = "30%",
@@ -56,8 +63,55 @@ navbuddy.setup {
     Operator      = kind.Operator,
     TypeParameter = kind.TypeParameter,
   },
-}
+  use_default_mappings = true,
+  mappings = {
+    ["<esc>"] = actions.close(), -- Close and cursor to original location
+    ["q"] = actions.close(),
 
-local opts = { silent = true, noremap = true }
--- not works
-vim.api.nvim_keymap_set("n", "<leader>dy", ":lua require('nvim-navbuddy').open()", opts)
+    ["j"] = actions.next_sibling(),     -- down
+    ["k"] = actions.previous_sibling(), -- up
+
+    ["h"] = actions.parent(),           -- Move to left panel
+    ["l"] = actions.children(),         -- Move to right panel
+    ["0"] = actions.root(),             -- Move to first panel
+
+    ["v"] = actions.visual_name(),      -- Visual selection of name
+    ["V"] = actions.visual_scope(),     -- Visual selection of scope
+
+    ["y"] = actions.yank_name(),        -- Yank the name to system clipboard "+
+    ["Y"] = actions.yank_scope(),       -- Yank the scope to system clipboard "+
+
+    ["i"] = actions.insert_name(),      -- Insert at start of name
+    ["I"] = actions.insert_scope(),     -- Insert at start of scope
+
+    ["a"] = actions.append_name(),      -- Insert at end of name
+    ["A"] = actions.append_scope(),     -- Insert at end of scope
+
+    ["r"] = actions.rename(),           -- Rename currently focused symbol
+
+    ["d"] = actions.delete(),           -- Delete scope
+
+    ["f"] = actions.fold_create(),      -- Create fold of current scope
+    ["F"] = actions.fold_delete(),      -- Delete fold of current scope
+
+    ["c"] = actions.comment(),          -- Comment out current scope
+
+    ["<enter>"] = actions.select(),     -- Goto selected symbol
+    ["o"] = actions.select(),
+
+    ["J"] = actions.move_down(), -- Move focused node down
+    ["K"] = actions.move_up(),   -- Move focused node up
+
+    ["t"] = actions.telescope({  -- Fuzzy finder at current level.
+      layout_config = {          -- All options that can be
+        height = 0.60,           -- passed to telescope.nvim's
+        width = 0.60,            -- default can be passed here.
+        prompt_position = "top",
+        preview_width = 0.50
+      },
+      layout_strategy = "horizontal"
+    }),
+
+    ["g?"] = actions.help(), -- Open mappings help window
+  },
+}
