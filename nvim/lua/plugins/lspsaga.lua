bc74d4010d12f07ef1e3ddfe4aa05847928960df
monji05 <monji4869@gmail.com>
2023-07-18 22:16:16 +0900

    modified lspsaga, mini.surround keymap

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -47,6 +47,6 @@ require("lspsaga").setup({
 })
 
 local opt = { silent = true, noremap = true }
-vim.keymap.set("n", "gd", "<CMD>Lspsaga lsp_finder<CR>", opt)
+vim.keymap.set("n", "gd", "<CMD>Lspsaga goto_definition<CR>", opt)
 vim.keymap.set("n", "gh", "<CMD>Lspsaga hover_doc<CR>", opt)
 vim.keymap.set("n", "<leader>j", "<CMD>Lspsaga diagnostic_jump_next<CR>", opt)
--- nvim/lua/plugins/mini-surround.lua
+++ nvim/lua/plugins/mini-surround.lua
@@ -13,7 +13,7 @@ require("mini.surround").setup( -- No need to copy this inside `setup()`. Will b
       delete = 'sd',         -- Delete surrounding
       find = 'sf',           -- Find surrounding (to the right)
       find_left = 'sF',      -- Find surrounding (to the left)
-      highlight = 'hs',      -- Highlight surrounding
+      highlight = 'Sh',      -- Highlight surrounding
       replace = 'sr',        -- Replace surrounding
       update_n_lines = 'sn', -- Update `n_lines`
 
