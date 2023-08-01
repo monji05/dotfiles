5d5da358a2bc6007aed4e6a6cb3d1aa7b4466352
monji05 <monji4869@gmail.com>
2023-08-01 23:15:07 +0900

    modified

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -44,7 +44,7 @@ end
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-local servers = { "tsserver", "marksman", "tailwindcss", "jdtls" }
+local servers = { "tsserver", "marksman", "tailwindcss", "jdtls", "json-lsp" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup({
     on_attach = on_attach,
