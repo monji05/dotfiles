5ed2168406b41f536b9bb9d9f380d286fa1b3bb1
monji05 <monji4869@gmail.com>
2023-08-10 21:54:04 +0900

    modified

--- nvim/lua/plugins/mini-surround.lua
+++ nvim/lua/plugins/mini-surround.lua
@@ -11,10 +11,10 @@ require("mini.surround").setup( -- No need to copy this inside `setup()`. Will b
     mappings = {
       add = 'sa',            -- Add surrounding in Normal and Visual modes
       delete = 'sd',         -- Delete surrounding
+      replace = 'sr',        -- Replace surrounding
       find = 'sf',           -- Find surrounding (to the right)
       find_left = 'sF',      -- Find surrounding (to the left)
       highlight = 'Sh',      -- Highlight surrounding
-      replace = 'sr',        -- Replace surrounding
       update_n_lines = 'sn', -- Update `n_lines`
 
       suffix_last = 'l',     -- Suffix to search with "prev" method
