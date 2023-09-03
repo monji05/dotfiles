28e01ffc9ca042493252665a53c1c17a7ae9a894
monji05 <monji4869@gmail.com>
2023-09-03 11:17:41 +0900

    remove phpactor's setup line in lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -70,18 +70,6 @@ nvim_lsp.lua_ls.setup({
   },
 })
 
--- nvim_lsp.phpactor.setup({
---   init_options = {
---     ["language_server_phpstan.enabled"] = false,
---     ["language_server_psalm.enabled"] = false,
---   },
---   capabilities = capabilities,
---   on_attach = function(client, bufnr)
---     navbuddy.attach(client, bufnr)
---   end,
---
--- })
-
 nvim_lsp.intelephense.setup({
   on_attach = function(client, bufnr)
     navbuddy.attach(client, bufnr)
