2a0a1861fe0a55687a284d7cf6447ea9e7e583d9
emishinaaaa <eriko.mishina@i-plug.co.jp>
2023-09-15 17:24:06 +0900

    modified telescope.live_grep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -70,8 +70,8 @@ telescope.setup({
       -- define mappings, e.g.
       mappings = {         -- extend mappings
         i = {
-          ["<C-q>"] = lga_actions.quote_prompt(),
-          ["<C-Q>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+          -- ["<C-I>"] = lga_actions.quote_prompt(),
+          ["<C-i>"] = lga_actions.quote_prompt({ postfix = " -ig !**/tests/**" }),
         },
       },
     },
