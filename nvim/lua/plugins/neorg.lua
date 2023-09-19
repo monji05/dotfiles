c14c57a785e0a71d0af6d52c45f62d64f4713bb3
emishinaaaa <eriko.mishina@i-plug.co.jp>
2023-09-19 15:09:05 +0900

    modified

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -13,18 +13,19 @@ require("neorg").setup({
     ["core.export"] = {
       config = {
         public = {
-          export_dir = "output"
-        }
+          export_dir = "output",
+        },
       },
     },
     ["external.exec"] = {},
-    ["core.integrations.telescope"] = {},
+    -- ["core.integrations.telescope"] = {},
     ["external.context"] = {},
-    ["external.templates"] = {
-      config = {
-        templates_dir = "~/neorg_notes/templates"
-      }
-    },
+
+    -- ["external.templates"] = {
+    --   config = {
+    --     templates_dir = "~/neorg_notes/templates"
+    --   }
+    -- },
     ["core.completion"] = {
       config = { engine = "nvim-cmp" },
     },
@@ -40,9 +41,9 @@ local neorg_callbacks = require("neorg.core.callbacks")
 neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
   -- Map all the below keybinds only when the "norg" mode is active
   keybinds.map_event_to_mode("norg", {
-    n = { -- Bind keys in normal mode
-      { "<C-F>", "core.integrations.telescope.find_linkable" },
-    },
+    -- n = { -- Bind keys in normal mode
+    --   { "<C-F>", "core.integrations.telescope.find_linkable" },
+    -- },
 
     i = { -- Bind in insert mode
       { "<C-p>", "core.integrations.telescope.insert_link" },
