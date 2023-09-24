8c943f17b2e50a76270f7aca81c83a0ed64fc6e8
monji05 <monji4869@gmail.com>
2023-09-24 11:45:21 +0900

    swap復活
    引数の入れ替えが簡単にできる

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -31,6 +31,14 @@ ts.setup({
   textobjects = {
     select = true,
     lookahead = true,
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+        ["<leader>A"] = "@parameter.inner",
+      }
+
+    },
     move = {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
