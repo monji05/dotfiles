97d6b7291f3d2f7cfe66c469ef6ee703befb3503
monji05 <monji4869@gmail.com>
2023-10-29 10:19:24 +0900

    modified gitsigns to use stage_hunk

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -48,7 +48,9 @@ require("gitsigns").setup({
 
     -- Text object
     map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-    map("n", "<Space>d", ":Gitsigns diffthis<CR>")
-    map("n", "<Space>h", gs.preview_hunk)
+    map({ "v", "n" }, "<Leader>sh", ":Gitsigns stage_hunk<CR>")
+    map({ "v", "n" }, "<Leader>dh", ":Gitsigns undo_stage_hunk<CR>")
+    -- map("n", "<Space>d", ":Gitsigns diffthis<CR>")
+    -- map("n", "<Space>h", gs.preview_hunk)
   end,
 })
