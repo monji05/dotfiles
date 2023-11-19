2f369865ab9b8a71534fdd59631e69200a65199e
monji05 <monji4869@gmail.com>
2023-11-19 11:33:04 +0900

    enable gitsigns next,prev hunk using ]c, [c and dashboard

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -13,6 +13,12 @@ return {
         "scss",
         "sql",
       })
+      vim.list_extend(opts.textobjects, {
+        goto_next_start = { ["]f"] = "@function.outer" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+        goto_previous_start = { ["[f"] = "@function.outer" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+      })
     end,
   },
 }
