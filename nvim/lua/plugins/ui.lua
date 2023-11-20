76d4ca2bcbde19df4cacbdb1700e7f1c530f1ef3
monji05 <monji4869@gmail.com>
2023-11-21 07:20:02 +0900

    enable relative file path on lualine

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -133,4 +133,17 @@ return {
       },
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1,
+          },
+        },
+      },
+    },
+  },
 }
