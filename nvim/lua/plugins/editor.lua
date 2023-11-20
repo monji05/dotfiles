56e4f84d993dad1d8eabb93f4b4779ea49a575f6
emishinaaaa <eriko.mishina@i-plug.co.jp>
2023-11-21 08:41:16 +0900

    modified

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -27,6 +27,7 @@ return {
       local telescope = require("telescope")
       local fb_actions = telescope.extensions.file_browser.actions
       local actions = require("telescope.actions")
+      local lga_actions = require("telescope-live-grep-args.actions")
       opts = {
         defaults = {
           mappings = {
@@ -41,30 +42,42 @@ return {
           },
         },
         extensions = {
-          file_browser = {
-            initial_mode = "normal",
-            theme = "ivy",
-            display_stat = { date = false, size = false },
-            -- disables netrw and use telescope-file-browser in its place
-            hijack_netrw = true,
-            prompt_path = true,
-            mappings = {
-              -- your custom insert mode mappings
-              ["i"] = {
-                ["<C-w>"] = function()
-                  vim.cmd("normal vbd")
-                end,
-                ["<C-o>"] = actions.select_tab,
+          live_grep_args = {
+            auto_quoting = true,
+            mappings = { -- extend mappings
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " -ig !**/tests/**" }),
               },
-              ["n"] = {
-                -- your custom normal mode mappings
-                ["n"] = fb_actions.create,
-                ["o"] = actions.select_tab,
-                ["h"] = fb_actions.goto_parent_dir,
-                ["H"] = fb_actions.toggle_hidden,
-                ["/"] = function()
-                  vim.cmd("startinsert")
-                end,
+              n = {
+                ["i"] = lga_actions.quote_prompt({ postfix = " -ig !**/tests/**" }),
+              },
+            },
+            file_browser = {
+              initial_mode = "normal",
+              theme = "ivy",
+              display_stat = { date = false, size = false },
+              -- disables netrw and use telescope-file-browser in its place
+              hijack_netrw = true,
+              prompt_path = true,
+              mappings = {
+                -- your custom insert mode mappings
+                ["i"] = {
+                  ["<C-w>"] = function()
+                    vim.cmd("normal vbd")
+                  end,
+                  ["<C-o>"] = actions.select_tab,
+                },
+                ["n"] = {
+                  -- your custom normal mode mappings
+                  ["n"] = fb_actions.create,
+                  ["o"] = actions.select_tab,
+                  ["h"] = fb_actions.goto_parent_dir,
+                  ["H"] = fb_actions.toggle_hidden,
+                  ["/"] = function()
+                    vim.cmd("startinsert")
+                  end,
+                },
               },
             },
           },
