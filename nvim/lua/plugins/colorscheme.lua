26e2726a9d8d325836d059cb80d6b2d11289c339
monji05 <monji4869@gmail.com>
2023-12-09 17:08:54 +0900

    modified

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  event = { "BufReadPost", "BufAdd", "BufNewFile" },
-  opts = {
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
+  {
+
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    opts = {
+      -- Value is any valid attr-list value for `:help nvim_set_hl`
+      comments = { italic = true },
+      keywords = { italic = true },
+    },
   },
 }
