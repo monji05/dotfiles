4400ae2d62ff9b05c36cbd9b998144032db2f352
monji05 <monji4869@gmail.com>
2023-12-10 09:33:38 +0900

    add nightfly commented

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,5 @@
 return {
   {
-
     "craftzdog/solarized-osaka.nvim",
     lazy = true,
     event = { "BufReadPost", "BufAdd", "BufNewFile" },
@@ -10,4 +9,12 @@ return {
       keywords = { italic = true },
     },
   },
+  -- {
+  --   "bluz71/vim-nightfly-guicolors",
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     -- load the colorscheme here
+  --     vim.cmd([[colorscheme nightfly]])
+  --   end,
+  -- },
 }
