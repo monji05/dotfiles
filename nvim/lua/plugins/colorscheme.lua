6bc9140962cde7c5ecd6941b210343edf0f17421
monji05 <monji4869@gmail.com>
2024-05-06 12:00:24 +0900

    remove vim-nightfly-guicolors

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,14 +13,6 @@ return {
     end,
     on_highlights = function(hl, c) end,
   },
-  -- {
-  --   "bluz71/vim-nightfly-guicolors",
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     -- load the colorscheme here
-  --     vim.cmd([[colorscheme nightfly]])
-  --   end,
-  -- },
   {
     "folke/tokyonight.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
