2a862dea4fae55bc540b5f1eac3b6a79cc0bc854
monji05 <monji4869@gmail.com>
2024-06-22 20:53:20 +0900

    modified, need to install yazi for using yazi.nvim

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = true },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = true },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
