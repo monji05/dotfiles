5d63e99ce5e2d230d738a2f0aec67a834cede3b9
monji05 <monji4869@gmail.com>
2024-06-27 18:21:07 +0900

    add laravel blade tree-sitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -19,6 +19,24 @@ return {
         goto_previous_start = { ["[f"] = "@function.outer" },
         goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
       })
+
+      -- laravel blade syntax highlight
+      -- https://github.com/EmranMR/tree-sitter-blade/discussions/19
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+
+      vim.cmd([[" Set the *.blade.php file to be filetype of blade
+        augroup BladeFiletypeRelated
+          au BufNewFile,BufRead *.blade.php set ft=blade
+        augroup END
+      ]])
     end,
   },
 }
