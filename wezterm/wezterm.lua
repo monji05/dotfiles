1bad840276beb88fcf1b9dfad70709625c69bac6
monji05 <monji4869@gmail.com>
2024-07-03 20:48:06 +0900

    modified wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,11 +8,9 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.font = wezterm.font("PlemolJP35 Console NF")
-config.font_size = 20
+config.font_size = 21
 config.default_prog = { "/opt/homebrew/bin/fish" }
--- config.window_background_opacity = 0.85 -- for tokyonight
-config.window_background_opacity = 0.95 -- for solarized
--- config.window_background_opacity = 0.95 -- for solarized-osaka
+config.window_background_opacity = 0.9
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = false
 config.cell_width = 0.95
@@ -20,18 +18,19 @@ config.cell_width = 0.95
 
 -- solarized-osaka night colorscheme
 config.colors = {
-	foreground = "#c0caf5",
-
-	-- tokyonight
-	-- background = "#011628",
+	-- tokyonight night
+	background = "#011423",
+	-- tokyonight storm
+	-- background = "#1f2335",
 
 	-- solarized-osaka
-	background = "#00181a",
+	-- background = "#00181a",
+	background = "#001011",
 
 	-- solarized
 	-- background = "#002B36",
 	-- background = "#01222b",
-	cursor_bg = "#c0caf5",
+	cursor_bg = "#47FF9C",
 	cursor_border = "#c0caf5",
 	cursor_fg = "#1a1b26",
 	selection_bg = "#283457",
@@ -39,33 +38,6 @@ config.colors = {
 
 	ansi = { "#15161e", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#a9b1d6" },
 	brights = { "#414868", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#c0caf5" },
-
-	tab_bar = {
-		inactive_tab_edge = "#16161e",
-		background = "#191b28",
-		-- background = "#011628",
-		active_tab = {
-			fg_color = "#7aa2f7",
-			bg_color = "#1a1b26",
-		},
-
-		inactive_tab = {
-			fg_color = "#7aa2f7",
-			bg_color = "#16161e",
-		},
-		inactive_tab_hover = {
-			bg_color = "#16161e",
-			fg_color = "#7aa2f7",
-		},
-		new_tab_hover = {
-			fg_color = "#16161e",
-			bg_color = "#7aa2f7",
-		},
-		new_tab = {
-			fg_color = "#7aa2f7",
-			bg_color = "#191b28",
-		},
-	},
 }
 
 config.keys = {
