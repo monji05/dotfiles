cb36c126cc744b64fb110f2736a6cdb673085c18
monji05 <monji4869@gmail.com>
2024-07-13 09:12:21 +0900

    fix: typo

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -70,7 +70,7 @@ return {
   {
     "altermo/ultimate-autopair.nvim",
     event = { "InsertEnter", "CmdlineEnter" },
-    branch = "v0.6", --recomended as each new version will have breaking changes
+    branch = "v0.6", --recommended as each new version will have breaking changes
     opts = {
       bs = { -- *ultimate-autopair-map-backspace-config*
         single_delete = true,
