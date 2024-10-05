9b968724de0496b39cb8a436891ed82cd8b38f43
monji05 <monji4869@gmail.com>
2024-10-05 16:23:11 +0900

    remove unnecessary plugin

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -107,14 +107,4 @@ return {
     version = "*",
     config = true,
   },
-  {
-    "toppair/peek.nvim",
-    ft = { "markdown" },
-    build = "deno task --quiet build:fast",
-    config = function()
-      require("peek").setup()
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-  },
 }
