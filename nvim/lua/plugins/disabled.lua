68b4c35bc48144aa33cc842e1ed612adfd5e24e2
monji05 <monji4869@gmail.com>
2024-12-08 17:19:28 +0900

    modified disabled.lua

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -10,6 +10,4 @@ return {
   { "petertriho/cmp-git", enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
   { "folke/which-key.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  { "MunifTanjim/nui.nvim", enabled = false },
 }
