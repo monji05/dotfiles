2b45ae4e739ac63eea17b96d7fa71c2525a6d0dd
monji05 <monji4869@gmail.com>
2024-12-23 21:43:41 +0900

    modified lsp.lua by referencing TJ

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -61,5 +61,40 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "saghen/blink.cmp" },
+    opts = {
+      servers = {
+        lua_ls = {},
+        intelephense = {},
+      },
+    },
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+      for server, config in pairs(opts.servers) do
+        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end,
+  },
+  {
+    {
+      "saghen/blink.cmp",
+      dependencies = "rafamadriz/friendly-snippets",
+
+      version = "*",
+      opts = {
+        keymap = { preset = "default" },
+
+        appearance = {
+          use_nvim_cmp_as_default = true,
+          nerd_font_variant = "mono",
+        },
+
+        sources = {
+          default = { "lsp", "path", "snippets", "buffer" },
+        },
+      },
+      opts_extend = { "sources.default" },
+    },
   },
 }
