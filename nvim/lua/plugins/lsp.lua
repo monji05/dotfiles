e516a5f1e565a7deeea4a9a47988414e42ffe31c
emishinaaaa <eriko.mishina@i-plug.co.jp>
2024-12-26 11:01:43 +0900

    fix lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -98,17 +98,16 @@ return {
     "saghen/blink.cmp",
     dependencies = "rafamadriz/friendly-snippets",
 
-      version = "*",
-      opts = {
+    version = "*",
+    opts = {
 
-        appearance = {
-          use_nvim_cmp_as_default = true,
-          nerd_font_variant = "mono",
-        },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
 
-        sources = {
-          default = { "lsp", "path", "snippets", "buffer" },
-        },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
       },
     },
     opts_extend = { "sources.default" },
