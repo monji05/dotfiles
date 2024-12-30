1025f9deba8dc14700e00ddf3c23afcbf3946f58
monji05 <monji4869@gmail.com>
2024-12-30 16:33:07 +0900

    feat(ui): modified color scheme of vscode

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -248,7 +248,18 @@ return {
     "m-demare/hlargs.nvim",
     config = function()
       require("hlargs").setup({})
+      -- solarized-osaka
       vim.api.nvim_command([[highlight Hlargs guifg=#c94c16]])
     end,
+    -- vscode
+    -- config = function()
+    --   require("hlargs").setup({
+    --     color = "#9CDCFE",
+    --   })
+    -- end,
+  },
+  {
+    -- for vscode
+    -- "HiPhish/rainbow-delimiters.nvim",
   },
 }
