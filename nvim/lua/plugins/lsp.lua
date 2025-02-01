d99d551836ed96cfdc401e88eaa227c6940768a3
monji05 <monji4869@gmail.com>
2025-02-01 14:16:15 +0900

    add colorful-menu for completion menu

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -91,6 +91,25 @@ return {
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
       },
+      completion = {
+        menu = {
+          draw = {
+            -- We don't need label_description now because label and label_description are already
+            -- combined together in label by colorful-menu.nvim.
+            columns = { { "kind_icon" }, { "label", gap = 1 } },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+            },
+          },
+        },
+      },
     },
     opts_extend = { "sources.default" },
   },
