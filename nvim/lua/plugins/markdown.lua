dcdfef3554424793dc3137d71d0fbb3038442467
monji05 <monji4869@gmail.com>
2025-02-02 13:32:45 +0900

    remove slide.lua, move to markdown.lua

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -4,4 +4,16 @@ return {
     lazy = false,
     ft = "markdown",
   },
+  {
+    "mpas/marp-nvim",
+    config = function()
+      require("marp").setup({
+        port = 8080,
+        wait_for_response_timeout = 30,
+        wait_for_response_delay = 1,
+      })
+      vim.keymap.set("n", "<leader>mt", "<cmd>MarpToggle<cr>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<leader>ms", "<cmd>MarpStatus<cr>", { noremap = true, silent = true })
+    end,
+  },
 }
