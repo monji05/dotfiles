f4bb29724c7dd8217dee6c24d37e06860b44b32c
monji05 <monji4869@gmail.com>
2025-02-02 11:08:32 +0900

    remove peek.nvim because dont move to link

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,16 +1,5 @@
 return {
   {
-
-    "toppair/peek.nvim",
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    config = function()
-      require("peek").setup()
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-  },
-  {
     "OXY2DEV/markview.nvim",
     lazy = false,
     ft = "markdown",
