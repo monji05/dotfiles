f2d981afe1da4c74fce69e15e08af10ed36ff3c7
monji05 <monji4869@gmail.com>
2025-03-13 21:24:19 +0900

    replace markview.nvim to render-markdown.nvim

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
     "mpas/marp-nvim",
     config = function()
       require("marp").setup({
