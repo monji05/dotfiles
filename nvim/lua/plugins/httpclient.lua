04e0a23e5fbc361f35349a5c85838607b96bb8c6
er.-ko mishina <msn@er-konoMacBook-Air.local>
2025-04-25 20:40:19 +0900

    change: kulala.nvim for useful http client

--- nvim/lua/plugins/httpclient.lua
+++ nvim/lua/plugins/httpclient.lua
@@ -1,5 +1,14 @@
 return {
   -- Requires Neovim 0.10.0+ and cURL.
   "mistweaverco/kulala.nvim",
-  opts = {},
+  keys = {
+    { "<leader>Rs", desc = "Send request" },
+    { "<leader>Ra", desc = "Send all requests" },
+    { "<leader>Rb", desc = "Open scratchpad" },
+  },
+  ft = { "http", "rest" },
+  opts = {
+    -- your configuration comes here
+    global_keymaps = false,
+  },
 }
