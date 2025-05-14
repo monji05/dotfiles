aba3a76aebc28d3dd63b4cb7107db66b76dacf2b
er.-ko mishina <msn@er-konoMacBook-Air.local>
2025-05-14 22:26:03 +0900

    remove: it's no longer need

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -12,24 +12,6 @@ return {
     end,
   },
   {
-    -- Convenience file operations for neovim
-    "chrisgrieser/nvim-genghis",
-    { "stevearc/dressing.nvim", lazy = true },
-    config = function()
-      local keymap = vim.keymap.set
-      local genghis = require("genghis")
-      keymap("n", "<leader>yp", genghis.copyFilepath)
-      keymap("n", "<leader>yn", genghis.copyFilename)
-      -- keymap("n", "<leader>cx", genghis.chmodx)
-      keymap("n", "<leader>rf", genghis.renameFile)
-      keymap("n", "<leader>mf", genghis.moveAndRenameFile)
-      keymap("n", "<leader>nf", genghis.createNewFile)
-      keymap("n", "<leader>yf", genghis.duplicateFile)
-      -- keymap("n", "<leader>df", function() genghis.trashFile { trashLocation = "your/path" } end) -- default: "$HOME/.Trash".
-      -- keymap("x", "<leader>x", genghis.moveSelectionToNewFile)
-    end,
-  },
-  {
     "weirongxu/plantuml-previewer.vim",
     { "tyru/open-browser.vim", lazy = true },
     { "aklt/plantuml-syntax", lazy = true },
