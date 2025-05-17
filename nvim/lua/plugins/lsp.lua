d0eff02f1c48d03194a5d88b48263e3cf310a038
emishinaaaa <eriko.mishina@i-plug.co.jp>
2025-05-17 15:25:50 +0900

    feat: tabで候補を選択できるようにかつ、選択したあとTabとして入力できるように

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -98,7 +98,17 @@ return {
         preset = "default",
         ["<Up>"] = {},
         ["<Down>"] = {},
-        ["<Tab>"] = { "select_next", "fallback" },
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
         ["<S-Tab>"] = { "select_prev", "fallback" },
         ["<C-j>"] = { "select_next", "fallback" },
         ["<C-k>"] = { "select_prev", "fallback" },
