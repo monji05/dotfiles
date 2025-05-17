c8893fe95cafad86a7f6831627e5f632456e72d1
emishinaaaa <eriko.mishina@i-plug.co.jp>
2025-05-17 12:57:04 +0900

    fix: ちゃんとphpcbfによる自動フォーマットができるように

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,10 +1,6 @@
 return {
   {
     "stevearc/conform.nvim",
-    { "mason.nvim", lazy = true, keys = {
-      "Mason",
-      "MaonsInstall",
-    } },
     opts = {
       formatters_by_ft = {
         json = { "jq" },
