667aeddd77ad2ae447ac8eb16bb471a8cb222106
er.-ko mishina <msn@er-konoMacBook-Air.local>
2025-06-08 09:04:12 +0900

    add: enable toggle boolean, 'and' 'or' in dial.nvim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -115,13 +115,12 @@ return {
           augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
           augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
           augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
-        },
-
-        -- augends used when group with name `mygroup` is specified
-        mygroup = {
-          augend.integer.alias.decimal,
           augend.constant.alias.bool, -- boolean value (true <-> false)
-          augend.date.alias["%m/%d/%Y"], -- date (02/19/2022, etc.)
+          augend.constant.new({ -- && <-> ||
+            elements = { "&&", "||" },
+            word = false,
+            cyclic = true,
+          }),
         },
       })
     end,
