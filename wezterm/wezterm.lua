ef90e04e3ed7f4fee20921c8866f1e44b113f8b2
emishinaaaa <eriko.mishina@i-plug.co.jp>
2025-07-25 09:53:10 +0900

    wezterm font, opacity

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,8 +10,8 @@ config.font = wezterm.font("PlemolJP35 Console NF")
 
 config.default_prog = { "/opt/homebrew/bin/fish" }
 
-config.font_size = 17
-config.window_background_opacity = 0.9
+config.font_size = 16
+config.window_background_opacity = 0.85
 config.macos_window_background_blur = 30
 config.window_decorations = "RESIZE"
 
