ffbbcd0d4e2cec1614d6d77075e6ecb59f06c82a
er.-ko mishina <msn@er-konoMacBook-Air.local>
2025-10-11 21:57:32 +0900

    modified incline

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -40,12 +40,13 @@ return {
     event = "BufReadPre",
     priority = 1200,
     config = function()
+      ---@class solarized.palette
       local colors = require("solarized-osaka.colors").setup()
       require("incline").setup({
         highlight = {
           groups = {
-            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
-            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+            InclineNormal = { guibg = colors.magenta, guifg = colors.base04 },
+            InclineNormalNC = { guifg = colors.violet, guibg = colors.base03 },
           },
         },
         window = { margin = { vertical = 0, horizontal = 1 } },
@@ -85,5 +86,4 @@ return {
       },
     },
   },
-},
 }
