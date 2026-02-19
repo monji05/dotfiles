940b2762f665d37201296ea19d523ab329520b03
er.-ko mishina <msn@er-konoMacBook-Air.local>
2026-02-19 20:41:52 +0900

    change iceberg to solarized-osaka

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -9,8 +9,8 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    -- { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "solarized-osaka" } },
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "iceberg" } },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "solarized-osaka" } },
+    -- { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "iceberg" } },
     -- { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "vague" } },
     -- { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "solarized" } },
     -- { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "solarized" } },
