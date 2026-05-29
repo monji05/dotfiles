dcf21668d5c9ae5e90ecd29c57e3f042e0fac28b
emishinaaaa <eriko.mishina@i-plug.co.jp>
2026-05-29 09:40:06 +0900

    migrate keymap

--- nvim/lua/plugins/nvim-session.lua
+++ nvim/lua/plugins/nvim-session.lua
@@ -2,7 +2,15 @@ return {
   "folke/persistence.nvim",
   event = "BufReadPre", -- this will only start session saving when an actual file was opened
   opts = {
-    -- add any custom options here
+  },
+  keys = {
+    {
+      ";s",
+      desc = "restore session",
+      function()
+        require('persistence').load()
+      end,
+    }
   }
 }
 
