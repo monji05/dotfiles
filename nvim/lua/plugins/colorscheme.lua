2f8fd0c7458e51c0f27935fb30b38cbcc9b66fa5
monji05 <monji4869@gmail.com>
2026-07-06 21:01:05 +0900

    uncomment hlsargs setup

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -160,7 +160,7 @@ return {
       -- vim.api.nvim_command([[highlight Hlargs guifg=#b5bd68]])
 
       -- solarized-osaka
-      -- vim.api.nvim_command([[highlight Hlargs guifg=#c94c16]])
+      vim.api.nvim_command([[highlight Hlargs guifg=#c94c16]])
 
       -- tokyonight
       -- That is the same phpDoc argment color
