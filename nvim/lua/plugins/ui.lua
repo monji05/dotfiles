e5ad62718b5fb1a73b6e470be2c4babad1917713
emishinaaaa <eriko.mishina@i-plug.co.jp>
2026-07-15 14:55:12 +0900

    disable incline.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -133,100 +133,100 @@ return {
         },
     },
     -- filename
-    {
-        "b0o/incline.nvim",
-        event = "BufReadPre",
-        priority = 900,
-        config = function()
-            -- local colors = require("iceberg_palette").colors
-            local colors = require("solarized-osaka.colors").setup()
-            local devicons = require("nvim-web-devicons")
-            require("incline").setup({
-                highlight = {
-                    groups = {
-                        -- iceberg
-                        -- InclineNormal = { guifg = colors.magenta, guibg = colors.l_gray },
-                        -- InclineNormalNC = { guifg = colors.gray, guibg = colors.bg },
-
-                        -- solarized-osaka
-                        -- active
-                        InclineNormal = { guifg = colors.magenta, guibg = colors.violet900 },
-                        -- inactive
-                        InclineNormalNC = { guifg = colors.base00, guibg = colors.bg },
-                    },
-                },
-
-                render = function(props)
-                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":.:~")
-                    local ft_icon, ft_color = devicons.get_icon_color(filename)
-                    local separator = { "┊ ", guifg = colors.blue }
-
-                    local function get_git_diff()
-                        local icons = { removed = "-", changed = "~", added = "+" }
-                        local signs = vim.b[props.buf].gitsigns_status_dict
-                        local labels = {}
-                        if signs == nil then
-                            return labels
-                        end
-                        for name, icon in pairs(icons) do
-                            if tonumber(signs[name]) and signs[name] > 0 then
-                                table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
-                            end
-                        end
-
-                        if #labels > 0 then
-                            table.insert(labels, separator)
-                        end
-                        return labels
-                    end
-
-                    local function get_diagnostic_label()
-                        local icons = { error = " ", warn = " ", info = " ", hint = " " }
-                        local label = {}
-
-                        for severity, icon in pairs(icons) do
-                            local n = #vim.diagnostic.get(props.buf,
-                                { severity = vim.diagnostic.severity[string.upper(severity)] })
-                            if n > 0 then
-                                table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
-                            end
-                        end
-                        if #label > 0 then
-                            table.insert(label, separator)
-                        end
-                        return label
-                    end
-
-                    local function show_git_branch()
-                        local label = {}
-                        local git_branch = (vim.b[props.buf].gitsigns_head or "")
-                        if git_branch ~= "" then
-                            table.insert(label, { "  " .. git_branch .. " ", guifg = colors.red })
-                        end
-
-                        if #label > 0 then
-                            table.insert(label, separator)
-                        end
-
-                        return label
-                    end
-
-                    local is_modified = {}
-                    if vim.bo[props.buf].modified then
-                        table.insert(is_modified, { " ", guifg = colors.yellow })
-                    end
-                    return {
-                        { show_git_branch() },
-                        { get_diagnostic_label() },
-                        { get_git_diff() },
-                        { (ft_icon or "") .. " ", guifg = ft_color,                                            guibg = "none" },
-                        { filename,               gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
-                        { is_modified },
-                    }
-                end,
-            })
-        end,
-    },
+    -- {
+    --     "b0o/incline.nvim",
+    --     event = "BufReadPre",
+    --     priority = 900,
+    --     config = function()
+    --         -- local colors = require("iceberg_palette").colors
+    --         local colors = require("solarized-osaka.colors").setup()
+    --         local devicons = require("nvim-web-devicons")
+    --         require("incline").setup({
+    --             highlight = {
+    --                 groups = {
+    --                     -- iceberg
+    --                     -- InclineNormal = { guifg = colors.magenta, guibg = colors.l_gray },
+    --                     -- InclineNormalNC = { guifg = colors.gray, guibg = colors.bg },
+    --
+    --                     -- solarized-osaka
+    --                     -- active
+    --                     InclineNormal = { guifg = colors.magenta, guibg = colors.violet900 },
+    --                     -- inactive
+    --                     InclineNormalNC = { guifg = colors.base00, guibg = colors.bg },
+    --                 },
+    --             },
+    --
+    --             render = function(props)
+    --                 local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":.:~")
+    --                 local ft_icon, ft_color = devicons.get_icon_color(filename)
+    --                 local separator = { "┊ ", guifg = colors.blue }
+    --
+    --                 local function get_git_diff()
+    --                     local icons = { removed = "-", changed = "~", added = "+" }
+    --                     local signs = vim.b[props.buf].gitsigns_status_dict
+    --                     local labels = {}
+    --                     if signs == nil then
+    --                         return labels
+    --                     end
+    --                     for name, icon in pairs(icons) do
+    --                         if tonumber(signs[name]) and signs[name] > 0 then
+    --                             table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
+    --                         end
+    --                     end
+    --
+    --                     if #labels > 0 then
+    --                         table.insert(labels, separator)
+    --                     end
+    --                     return labels
+    --                 end
+    --
+    --                 local function get_diagnostic_label()
+    --                     local icons = { error = " ", warn = " ", info = " ", hint = " " }
+    --                     local label = {}
+    --
+    --                     for severity, icon in pairs(icons) do
+    --                         local n = #vim.diagnostic.get(props.buf,
+    --                             { severity = vim.diagnostic.severity[string.upper(severity)] })
+    --                         if n > 0 then
+    --                             table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
+    --                         end
+    --                     end
+    --                     if #label > 0 then
+    --                         table.insert(label, separator)
+    --                     end
+    --                     return label
+    --                 end
+    --
+    --                 local function show_git_branch()
+    --                     local label = {}
+    --                     local git_branch = (vim.b[props.buf].gitsigns_head or "")
+    --                     if git_branch ~= "" then
+    --                         table.insert(label, { "  " .. git_branch .. " ", guifg = colors.red })
+    --                     end
+    --
+    --                     if #label > 0 then
+    --                         table.insert(label, separator)
+    --                     end
+    --
+    --                     return label
+    --                 end
+    --
+    --                 local is_modified = {}
+    --                 if vim.bo[props.buf].modified then
+    --                     table.insert(is_modified, { " ", guifg = colors.yellow })
+    --                 end
+    --                 return {
+    --                     { show_git_branch() },
+    --                     { get_diagnostic_label() },
+    --                     { get_git_diff() },
+    --                     { (ft_icon or "") .. " ", guifg = ft_color,                                            guibg = "none" },
+    --                     { filename,               gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
+    --                     { is_modified },
+    --                 }
+    --             end,
+    --         })
+    --     end,
+    -- },
     -- icons
     {
         "nvim-mini/mini.icons",
