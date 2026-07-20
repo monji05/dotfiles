174ce0e88749fa0490320b033f3bce630d423ca9
monji05 <monji4869@gmail.com>
2026-07-20 17:15:51 +0900

    disable solarized-osaka

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -8,90 +8,90 @@ return {
   --     vim.cmd([[colorscheme vscode]])
   --   end,
   -- },
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    event = { "BufReadPre", "BufReadPost", "BufAdd", "BufNewFile" },
-    opts = {
-      day_brightness = 0.5,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = { italic = true },
-        floats = "transparent",
-        sidebars = "transparent",
-      },
-
-      ---@param colors ColorScheme
-      on_colors = function(colors)
-        colors.base0 = "#92a0a0"
-      end,
-      ---@param highlights Highlights
-      ---@param colors ColorScheme
-      on_highlights = function(hl, c)
-        local number_fg = c.violet500
-        -- hl["@number"] = {
-        --   fg = number_fg,
-        -- }
-        -- hl.Number = {
-        --   fg = number_fg,
-        -- }
-        -- hl["@lsp.type.number"] = {
-        --   fg = number_fg,
-        -- }
-        -- hl.LspKindNumber = {
-        --   fg = number_fg,
-        -- }
-        -- hl.LineNr = {
-        --   fg = c.base01,
-        --   -- bg = c.base02,
-        -- }
-        -- hl.CursorLineNr = {
-        --   fg = c.yellow300,
-        --   bg = c.yellow700,
-        -- }
-        -- hl.GitSignsAdd = {
-        --   fg = c.green500,
-        --   bg = c.base02,
-        -- }
-        -- hl.GitSignsChange = {
-        --   fg = c.yellow500,
-        --   bg = c.base02,
-        -- }
-        -- hl.GitSignsDelete = {
-        --   fg = c.red500,
-        --   bg = c.base02,
-        -- }
-        -- hl.DiagnosticHint = {
-        --   bg = c.green900,
-        --   fg = c.green500,
-        -- }
-        --
-        -- word_diff
-        hl.GitSignsAddLnInline = {
-          bg = c.green700,
-          -- fg = c.base01,
-        }
-        hl.GitSignsAddLn = {
-          bg = c.green700,
-        }
-        hl.GitSignsUntrackedLn = {
-          bg = c.magenta,
-        }
-        hl.GitSignsChangeLn = {
-          bg = c.yellow900,
-          -- fg = c.base01,
-        }
-        hl.GitSignsChangeLnInline = {
-          bg = c.yellow700,
-        }
-      end,
-    },
-    config = function()
-      vim.cmd([[colorscheme solarized-osaka]])
-    end,
-  },
+  -- {
+  --   "craftzdog/solarized-osaka.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   event = { "BufReadPre", "BufReadPost", "BufAdd", "BufNewFile" },
+  --   opts = {
+  --     day_brightness = 0.5,
+  --     styles = {
+  --       comments = { italic = true },
+  --       keywords = { italic = true },
+  --       functions = { italic = true },
+  --       floats = "transparent",
+  --       sidebars = "transparent",
+  --     },
+  --
+  --     ---@param colors ColorScheme
+  --     on_colors = function(colors)
+  --       colors.base0 = "#92a0a0"
+  --     end,
+  --     ---@param highlights Highlights
+  --     ---@param colors ColorScheme
+  --     on_highlights = function(hl, c)
+  --       local number_fg = c.violet500
+  --       -- hl["@number"] = {
+  --       --   fg = number_fg,
+  --       -- }
+  --       -- hl.Number = {
+  --       --   fg = number_fg,
+  --       -- }
+  --       -- hl["@lsp.type.number"] = {
+  --       --   fg = number_fg,
+  --       -- }
+  --       -- hl.LspKindNumber = {
+  --       --   fg = number_fg,
+  --       -- }
+  --       -- hl.LineNr = {
+  --       --   fg = c.base01,
+  --       --   -- bg = c.base02,
+  --       -- }
+  --       -- hl.CursorLineNr = {
+  --       --   fg = c.yellow300,
+  --       --   bg = c.yellow700,
+  --       -- }
+  --       -- hl.GitSignsAdd = {
+  --       --   fg = c.green500,
+  --       --   bg = c.base02,
+  --       -- }
+  --       -- hl.GitSignsChange = {
+  --       --   fg = c.yellow500,
+  --       --   bg = c.base02,
+  --       -- }
+  --       -- hl.GitSignsDelete = {
+  --       --   fg = c.red500,
+  --       --   bg = c.base02,
+  --       -- }
+  --       -- hl.DiagnosticHint = {
+  --       --   bg = c.green900,
+  --       --   fg = c.green500,
+  --       -- }
+  --       --
+  --       -- word_diff
+  --       hl.GitSignsAddLnInline = {
+  --         bg = c.green700,
+  --         -- fg = c.base01,
+  --       }
+  --       hl.GitSignsAddLn = {
+  --         bg = c.green700,
+  --       }
+  --       hl.GitSignsUntrackedLn = {
+  --         bg = c.magenta,
+  --       }
+  --       hl.GitSignsChangeLn = {
+  --         bg = c.yellow900,
+  --         -- fg = c.base01,
+  --       }
+  --       hl.GitSignsChangeLnInline = {
+  --         bg = c.yellow700,
+  --       }
+  --     end,
+  --   },
+  --   config = function()
+  --     vim.cmd([[colorscheme solarized-osaka]])
+  --   end,
+  -- },
   {
     "m-demare/hlargs.nvim",
     config = function()
