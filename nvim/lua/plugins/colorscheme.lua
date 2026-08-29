12a08634faadff2998eace2d9e3096b136729065
monji05 <monji4869@gmail.com>
2026-08-29 20:26:02 +0900

    change vivid_brightnessに合わせてhlargsも変更

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -105,7 +105,7 @@ return {
       -- vim.api.nvim_command([[highlight Hlargs guifg=#b5bd68]])
 
       -- solarized-osaka
-      vim.api.nvim_command([[highlight Hlargs guifg=#c94c16]])
+      vim.api.nvim_command([[highlight Hlargs guifg=#db4b4b]])
 
       -- tokyonight
       -- That is the same phpDoc argment color
